281e17136035c3d29966efa8ae68fd8e1c842d50
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2025-10-16 17:52:37 +0200

    Change Firebase service account to use env variable
    Updated service account initialization to use environment variable.

--- api.js
+++ api.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require( './serviceAccountKey.json'); // Your downloaded key
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+//const serviceAccount = require( './serviceAccountKey.json'); // Your downloaded key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static HTML files
-app.use(express.static(path.join(__dirname, '..')));
+//app.use(express.static(path.join(__dirname, '..')));
 
 // Import and mount API modules
 const aboutApi = require('./pages/about');
@@ -41,4 +41,5 @@ app.use('/sundaySermon', sundaySermonApi);
 
 // Start the server
 app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)});
+
 
