771c56a8d7288a0c4cecc57976f36a3a86ede4d8
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2025-10-16 17:47:48 +0200

    Revert service account initialization method

--- api.js
+++ api.js
@@ -3,9 +3,8 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const path = require('path');
 
-// Initialize Firebase Admin
-// const serviceAccount = require( './serviceAccountKey.json'); // Your downloaded key
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require( './serviceAccountKey.json'); // Your downloaded key
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -22,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 // Serve static HTML files
-// app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Import and mount API modules
 const aboutApi = require('./pages/about');
@@ -42,3 +41,4 @@ app.use('/sundaySermon', sundaySermonApi);
 
 // Start the server
 app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)});
+
